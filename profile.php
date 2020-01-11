472dffe4d5a3c66028354573bf541419921ca4fa
Acbegley <44815145+Acbegley@users.noreply.github.com>
2020-01-11 17:58:14 -0500

    Add files via upload

--- profile.php
+++ profile.php
@@ -55,19 +55,34 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 }
 require_once "config.php";
 $sql = "SELECT score FROM users WHERE id = ?";
-$stmt = mysqli_prepare($link, $sql))
-mysqli_stmt_bind_param($stmt, "si", $param_id);
-$param_id = $_SESSION["id"];
-$result = $link->query($sql);
+if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "si", $param_id);
+            
+            // Set parameters
+            $param_id = $_SESSION["id"];
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                //session_destroy();
+                //header("location: login.php");
+                //exit();
+				$result = $link->query($sql);
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) {
+						echo "<br> score: ". $row["score"]. "<br>";
+					}
+				} else {
+					echo "0 results";
+				}
+							} else{
+								echo "Oops! Something went wrong. Please try again later.";
+						}
+mysqli_stmt_close($stmt);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> score: ". $row["score"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+
 ?>
 </body>
 </html>
