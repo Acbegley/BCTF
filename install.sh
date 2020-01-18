089def76c0ea2352013eb39ce60db9524e53f5f2
Acbegley <44815145+Acbegley@users.noreply.github.com>
2020-01-18 11:24:40 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt-get install apache2 -y
+sudo apt-get install mysql-server -y
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+sudo mv *  /var/www/html/
+cd .. && sudo rm -r BCTF/
+cd /var/www/html
+sudo service mysql resatrt
+sudo mysql -u root -p ctf < ctf.sql
+service apache2 restart
+sudo rm ctf.sql
