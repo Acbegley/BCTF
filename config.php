d408c18943547bdb640e8946a9018e433b25e6c3
Acbegley <44815145+Acbegley@users.noreply.github.com>
2020-01-18 11:38:00 -0500

    Add files via upload

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'database');
+define('DB_PASSWORD', 'P@ssw0rd');
+define('DB_NAME', 'ctf');
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
