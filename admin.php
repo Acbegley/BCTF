0c47e53f9c640d7f4112b389bea55ad9151dcb8d
Acbegley <44815145+Acbegley@users.noreply.github.com>
2020-02-04 10:16:58 -0500

    Add files via upload

--- admin.php
+++ admin.php
@@ -45,10 +45,11 @@ body {
 
 <div class="topnav">
   <a class="active" href="welcome.php">Home</a>
-  <a href="profile.php">Profile</a>
   <?php
   require_once "config.php";
   $username = $_SESSION["username"];
+  $id = $_SESSION["id"];
+  echo "<a href="profile.php?id=$id">Profile</a>"
   $sql = "SELECT admin FROM users WHERE username = '$username'";
   $query = mysqli_query($link, $sql);
   while($rs = mysqli_fetch_assoc($query)){
