5747821e8f36638c3263e6a248b6d3b8d882c162
Acbegley <44815145+Acbegley@users.noreply.github.com>
2020-04-26 16:46:54 -0400

    Add files via upload

--- ctf.sql
+++ ctf.sql
@@ -53,11 +53,13 @@ DROP TABLE IF EXISTS `completion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `completion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
   `challengeName` varchar(256) DEFAULT NULL,
   `attemptsUsed` int(11) DEFAULT NULL,
   `hintUsed` int(11) DEFAULT NULL,
-  `complete` int(11) DEFAULT NULL
+  `complete` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -108,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-24 15:38:27
+-- Dump completed on 2020-04-26 16:46:14
