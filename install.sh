18aa0be079397d3403d3b85c81d38187f2d8ce54
Acbegley <44815145+Acbegley@users.noreply.github.com>
2020-05-01 19:05:57 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install apache2 -y
 sudo apt-get install mysql-server -y
-sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql php-ldap
 sudo mv *  /var/www/html/
 cd .. && sudo rm -r BCTF/
 cd /var/www/html
