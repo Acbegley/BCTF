c67934c9b5e58613d4803a008e3e05470211a987
Acbegley <44815145+Acbegley@users.noreply.github.com>
2020-05-18 20:38:30 -0400

    Add files via upload

--- register.php
+++ register.php
@@ -22,7 +22,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 if(mysqli_stmt_num_rows($stmt) == 1){
                     $username_err = "This username is already taken.";
                 } else{
-                    $username = trim($_POST["username"]);
+                    $username = strip_tags(trim($_POST["username"]));
                 }
             } else{
                 echo "Oops! Something went wrong. Please try again later.";
